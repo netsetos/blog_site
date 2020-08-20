b8c59ce2729073bf7bbd8fad7649e356c46ad944
Netsetos Tech <50771820+netsetos@users.noreply.github.com>
2020-08-20 21:45:22 +0530

    blog
    Initial File

---
+++ netsetosblog/blog/feeds.py
@@ -0,0 +1,18 @@
+from django.contrib.syndication.views import Feed
+from django.template.defaultfilters import truncatewords
+from django.urls import reverse_lazy
+from .models import Post
+
+class LatestPostsFeed(Feed):
+    title = 'Netsetos Tutorials'
+    link = reverse_lazy('blog:post_list')
+    description = 'New Post of my Blog'
+
+    def items(self):
+        return Post.published.all()[:5]
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return truncatewords(item.body,30)
