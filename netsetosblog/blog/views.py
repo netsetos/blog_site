186a96a5ab167a1453222527294808486fedad96
Netsetos Tech <50771820+netsetos@users.noreply.github.com>
2020-08-20 21:46:32 +0530

    More
    Initial

---
+++ netsetosblog/blog/views.py
@@ -0,0 +1,101 @@
+from django.shortcuts import render,get_object_or_404
+from .models import Post, Comment
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm,CommentForm,SearchForm
+from django.core.mail import send_mail
+from taggit.models import Tag
+from django.db.models import Count
+from django.contrib.postgres.search import SearchVector,SearchQuery,SearchRank
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title','body')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(
+                search = search_vector,
+                rank = SearchRank(search_vector,search_query)
+            ).filter(search=search_query).order_by('-rank')
+    return render(request,'blog/post/search.html',
+                      {'form':form,'query':query,'results':results})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+def post_list(request,tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag,slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+    paginator = Paginator(object_list,3)# 3 posts at single page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request,'blog/post/list.html',{'page':page,'posts':posts,'tag':tag})
+
+#To display a single post
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, status='published',
+                             publish__year=year, publish__month=month,
+                             publish__day=day)
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
+
+    return render(request, 'blog/post/detail.html',
+                  {'post':post,
+                   'comments':comments,
+                   'new_comment':new_comment,
+                   'comment_form':comment_form,
+                   'similar_posts':similar_posts})
+
+def post_share(request,post_id):
+    post = get_object_or_404(Post,id=post_id,status='published')
+    sent = False
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            #.....send email
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read" f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject,message,'netsetos@gmail.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request,'blog/post/share.html',{'post':post,
+                                                  'form':form,
+                                                  'sent':sent})
+
+
+
+
